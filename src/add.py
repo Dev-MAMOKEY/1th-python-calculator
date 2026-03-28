f2277d437f363b541cd58dd555893016dd6b08b4
127lucas <12lucaslee@gmail.com>
2026-03-28 16:45:35 +0900

    Feat/덧셈기능 함수화 적용

--- src/add.py
+++ src/add.py
@@ -1,8 +1,2 @@
-num1 = int(input("첫번째 숫자 입력:"))
-연산 = input("연산 선택 (+ - * /):") 
-num2 = int(input("두번째 숫자 입력:"))
-
-if 연산 == "+":
-    print("결과:", num1 + num2)
-else:
-    print("잘못된 입력입니다")
+def add(num1, num2):
+    return int(num1) + int(num2)
